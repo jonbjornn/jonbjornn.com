e924bb246026ecd97aa16d90cbff40043e4fb1c3
Jon Bjornn <jonbjornn@gmail.com>
2015-12-17 01:38:14 -0800

    add in little animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,22 @@
 //= require_tree .
+
+
+$(document).ready(function() {
+  var mouseX = $(window).width()/2,
+    mouseY = $(window).height()/2;
+  $(document).mousemove(function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+  });
+  var follower = $('em');
+  var xp = 0,
+    yp = 0;
+  var loop = setInterval(function() {
+    xp += (mouseX - xp) / 30;
+    yp += (mouseY - yp) / 30;
+    follower.css({
+      left: xp,
+      top: yp
+    });
+  }, 30);
+});
